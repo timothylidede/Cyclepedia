d3e4481144318783d706346571b5f8dd4838b020
Leo <leo.bikuri@strathmore.edu>
2022-05-02 01:29:01 +0300

    small deletes

--- server/api/auth.js
+++ server/api/auth.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const cors = require('cors');
-const { json } = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
-const app = express();
 router.use(express.json());
 
 router.post("/api/register", async (request, response) => {
