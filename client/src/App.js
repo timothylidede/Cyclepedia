5a739896d67ab0a8bc4e820fbd799047789ed717
Who else? <leo.bikuri@strathmore.edu>
2022-05-04 19:59:57 +0300

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import React from 'react';
 import  { BrowserRouter, Route, Routes } from 'react-router-dom'
