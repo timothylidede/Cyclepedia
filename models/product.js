512f9da991a2fd93ffa77e7a8d4f4db4ca07c9e7
timothylidede <75303411+timothylidede@users.noreply.github.com>
2022-05-07 19:45:13 +0300

    Model Product addition

--- models/product.js
+++ models/product.js
@@ -5,19 +5,83 @@ const Product = new mongoose.Schema(
     product_name: {
       type: String,
       required: [true, "Product name is required"],
+      trim: true,
+      maxLength: [100, "Product name cannot exceed 100 characters"]
     },
     price: {
       type: Number,
       required: [true, "Price is required"],
+      maxLength: [8],
+      default: [0.0]
     },
     description: {
       type: String,
       required: [true, "Product description is required"],
     },
-    image: {
-      type: String,
-      required: [true, "Product description is required"],
+    ratings: {
+      type: Number,
+      default: 0
     },
+    images: [
+      {
+        public_id:{
+          type: String,
+          required: true
+        },
+        url:{
+          type: String,
+          required: true
+        },
+      }
+    ],
+    category: {
+      type: String,
+      required: [true, "Product category is required"],
+      enum: {
+        values: [
+          "Cruiser",
+          "Sport",
+          "Touring",
+          "Standard",
+          "Dual-Purpose",
+          "Dirt"
+        ],
+        message: "Select correct product category",
+      }
+    },
+    seller: {
+      type: String,
+      required: [true, "Enter product seller"],
+
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0
+    },
+    reviews: [
+      {
+        user_id: {
+          type: String,
+          required: true
+        },
+        name: {
+          type: String,
+          required: true
+        },
+        rating: {
+          type: Number,
+          required: true
+        },
+        comment: {
+          type: String,
+          required: true
+        }
+      }
+    ],
+    cretedAt: {
+      type: Date,
+      default: Date.now
+    }
   },
   { timestamps: true },
   { collecton: "products" }
