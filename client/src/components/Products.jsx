2a77f83bab46499bbb8bc0d1ede092eed41439de
Baranaba <baranaba@vokali.com>
2022-06-01 14:35:44 +0300

    working on displaying dynamic product

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -1,24 +1,24 @@
 import styled from "styled-components";
-import {popularProducts} from "../data";
+import { popularProducts } from "../data";
 import Product from "./Product";
 import React from "react";
 
 const Container = styled.div`
-      padding: 20px;
-      width: 100%;
-      display: flex;
-      flex-basis: 400px ;
-      flex-wrap: wrap;
-      justify-content: space-between;
-`
+  padding: 20px;
+  width: 100%;
+  display: flex;
+  flex-basis: 400px;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
 const Products = () => {
-    return (
-        <Container>
-            {popularProducts.map(item=>(
-                <Product item={item} key={item.id} />
-            ))}
-        </Container>
-    );
-}
+  return (
+    <Container>
+      {popularProducts.map((item) => (
+        <Product item={item} key={item.id} />
+      ))}
+    </Container>
+  );
+};
 
 export default Products;
