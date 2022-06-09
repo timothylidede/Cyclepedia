6a60cea338b02ede0a3f5d67cb3b30d0fcf52897
Leo-Bikuri <leo.bikuri@strathmore.edu>
2022-06-09 17:11:09 +0300

    Fixed Private routes

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,21 +1,15 @@
- import {Navigate, Route} from 'react-router-dom';
+ import {Navigate, Outlet} from 'react-router-dom';
  import React from "react";
 
 const PrivateRoute = ({component: Component, ...rest}) => {
     return(
-        <Route 
-           {...rest} 
-            render = {
-                (props) => 
-                    localStorage.getItem('authToken') ? (
-                        <Component {...props} />
-                    ) : (
-                       <Navigate to="/login" />
-                    )
-                
-            }
-        />
-    )
-}
+        localStorage.getItem('authToken') ? (
+            <Outlet />
+        ) : (
+            <Navigate to="/login" />
+        )               
 
-export default PrivateRoute
+    );
+};
+
+export default PrivateRoute;
